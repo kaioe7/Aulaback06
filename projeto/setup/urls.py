4ffa2decfc113516bfdaab0b4d68378dc5dd554e
unknown <oliveiraabreu171@gmail.com>
2025-03-14 11:16:45 -0300

    templates

--- projeto/setup/urls.py
+++ projeto/setup/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from todos.views import home
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home)
 ]
